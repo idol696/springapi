121bc19af283512a2f8b936e49a20e4666f09a62
Roman Kuzmin <ghelatin@mail.ru>
2024-08-28 19:29:11 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/springapi/SpringapiApplication.java
@@ -0,0 +1,14 @@
+package ru.prostostudia.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringapiApplication.class, args);
+	}
+
+}
